ff0a7f1cfb5cdf53db170502797516f1928291d1
Henrian Lorico <162807296+loricohenrian@users.noreply.github.com>
2024-11-26 10:25:56 +0800

    Update fvalidation.py

--- finalproject/fvalidation.py
+++ finalproject/fvalidation.py
@@ -1,4 +1,3 @@
-#fvalidation.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, EmailField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
