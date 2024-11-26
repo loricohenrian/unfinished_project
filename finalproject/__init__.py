e7546809f01b00766c927e143d990a2c468cb707
Henrian Lorico <162807296+loricohenrian@users.noreply.github.com>
2024-11-26 10:26:43 +0800

    Update __init__.py

--- finalproject/__init__.py
+++ finalproject/__init__.py
@@ -1,19 +1,15 @@
 from flask import Flask
-from flask_mysql_connector import MySQL
-#__init__.py
+from flask_mysql_connector import MySQ
 app = Flask(__name__)
 
-# Secret key for session management
 app.config['SECRET_KEY'] = '30a9c161f25a46f1d97998c10bd0b1f8'
 app.config['WTF_CSRF_ENABLED'] = True
 
-# MySQL configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''  # Update this with your MySQL password if needed
+app.config['MYSQL_PASSWORD'] = ''  
 app.config['MYSQL_DATABASE'] = 'flask_db'
 
-# Initialize MySQL
 mysql = MySQL(app)
 
-from finalproject import routes  # Import routes after MySQL setup
+from finalproject import routes
